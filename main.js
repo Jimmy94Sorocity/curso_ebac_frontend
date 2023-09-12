d8e80bb9d0cb4aee86ff89dec115e8f753e9f91e
Matheus Fré <matheusfre94@gmail.com>
2023-09-12 08:58:44 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("form").on("submit", function(e) { 
+        e.preventDefault();
+
+        const nome_tarefa = $("#nome-tarefa").val(); 
+
+        $(`<li>${nome_tarefa}</li>`).appendTo("ul"); 
+        $("#nome-tarefa").val("");
+
+        $("ul").on("click","li",function() {
+            $("li").css("text-decoration", "line-through");;
+        })
+    });
+});
